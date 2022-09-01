7613a83f6905a31d9d1a0e4a8280b374c6e254cb
Dimitar Prisadnikov <dimitarprisadnikov@studyportals.com>
2022-09-01 11:31:57 +0200

    added new spring boot project

---
+++ spotforus/src/test/java/nl/fontys/s3/spotforus/SpotforusApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.s3.spotforus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotforusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
