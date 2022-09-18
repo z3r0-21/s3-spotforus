d6af52fc617531d2cbdf167ae80796b70a333c9a
Dimitar Prisadnikov <d.prisadnikov@student.fontys.n>
2022-09-18 23:10:30 +0200

    test

--- spotforus/src/main/java/nl/fontys/s3/spotforus/repositories/HouseholdRepository.java
+++ spotforus/src/main/java/nl/fontys/s3/spotforus/repositories/HouseholdRepository.java
@@ -2,7 +2,9 @@ package nl.fontys.s3.spotforus.repositories;
 
 import nl.fontys.s3.spotforus.entities.Household;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HouseholdRepository extends JpaRepository<Household, Long> {
 
 }
