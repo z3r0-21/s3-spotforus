9c807dea6541a026df1f25bb18da4650d171e7ce
Dimitar Prisadnikov <d.prisadnikov@student.fontys.n>
2022-09-18 22:54:53 +0200

    test fix

--- spotforus/src/test/java/nl/fontys/s3/spotforus/SpotforusApplicationTests.java
+++ spotforus/src/test/java/nl/fontys/s3/spotforus/SpotforusApplicationTests.java
@@ -2,13 +2,15 @@ package nl.fontys.s3.spotforus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SpotforusApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test() {
 		assertEquals(11,11);
 	}
 
